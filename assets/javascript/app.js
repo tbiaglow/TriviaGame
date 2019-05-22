d4b6540b33fc7642ad3e03020655740182160197
tbiaglow <tbiaglow@gmail.com>
2019-05-22 01:18:40 -0400

    determineScore function now works properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,74 @@
 
 var time = 5;
 $("#display").html("00:05");
-var questions = [question1, question2, question3, question4, question5];
 
-var userAnswers = [];
-//var correctAnswers = [questions[0].answerB, questions[1].answerC, questions[2].answerA, questions[3].answerA, questions[4].answerD];
+
 
 var question1 = {
     question: "",
     answerA: "",
-    answerB: "",
-    AnswerC: "",
-    AnswerD: ""
+    answerB: 1,
+    answerC: "",
+    answerD: ""
 };
 var question2 = {
     question: "",
     answerA: "",
     answerB: "",
-    AnswerC: "",
-    AnswerD: ""
+    answerC: 2,
+    answerD: ""
 };
 var question3 = {
     question: "",
-    answerA: "",
+    answerA: 3,
     answerB: "",
-    AnswerC: "",
-    AnswerD: ""
+    answerC: "",
+    answerD: ""
 };
 var question4 = {
     question: "",
-    answerA: "",
+    answerA: 4,
     answerB: "",
-    AnswerC: "",
-    AnswerD: ""
+    answerC: "",
+    answerD: ""
 };
 var question5 = {
     question: "",
     answerA: "",
     answerB: "",
-    AnswerC: "",
-    AnswerD: ""
+    answerC: "",
+    answerD: 5
 };
+
+var questions = [question1, question2, question3, question4, question5];
+var correctAnswers = [questions[0].answerB, questions[1].answerC, questions[2].answerA, questions[3].answerA, questions[4].answerD];
+
+
+var userAnswers = [1, 2, 3, 4, 5];
 
 var gameInfo = $('#wrapper')
 
 intervalID = setInterval(count, 1000);
 
-score = "88%"
-// function determineScore(userAnswers, correctAnswers) {
-//   return score
-// }
+
+function determineScore(userAnswers, correctAnswers) {
+  var score = 0;
+  for (i = 0; i < correctAnswers.length; i++) {
+    if (userAnswers[i] === correctAnswers[i]) {
+      score += 1 / correctAnswers.length;
+      
+    }
+  }
+  score = Math.round(score * 100)
+  return score
+}
 
 function gameOver() {
+  score = determineScore(userAnswers, correctAnswers);
   $('#wrapper').remove()
   $('body').append('<div id="score">Your score was: </div>')
   $("#score").append(score)
+  $("#score").append("%")
   $('body').append('<div id=playAgain>Play Again? </div>')
   $('#playAgain').append('<button id="playButton">Play</button>')
   $('#playButton').click(function (){startNewGame()})
